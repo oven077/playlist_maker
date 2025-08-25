bb65612a4ad740f19485f0d293116d8ce40b61f2
Андрей Ермолин <agermolin@yarus.local>
2025-08-25 18:48:57 +0300

    feat: + action on settings layout

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.playlistmaker
+
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,6 +11,10 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "onCreate: MainActivity создана $this")
+
+
         setContentView(R.layout.activity_main)
 
 
@@ -23,11 +29,23 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.btn_settings).setOnClickListener {
             navigateTo(SettingsActivity::class.java)
         }
-
-
     }
 
     private fun navigateTo(activityClass: Class<out Activity>) {
         startActivity(Intent(this@MainActivity, activityClass))
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"MainActivity старт $this")
+    }
+
+
+
+
+
+    companion object {
+        private const val TAG = "asd"
+    }
+
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
@@ -10,6 +13,28 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
         findViewById<Toolbar>(R.id.settings_toolbar).setNavigationOnClickListener() {
             finish()
+        }
+
+        findViewById<Button>(R.id.button_sharing).setOnClickListener() {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_link))
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, null))
+        }
+
+        findViewById<Button>(R.id.button_support).setOnClickListener() {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_address)))
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.email_message))
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_theme))
+            startActivity(Intent.createChooser(intent, null))
+        }
+
+        findViewById<Button>(R.id.button_user_agreement).setOnClickListener() {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(getString(R.string.support_user_agreement))
+            startActivity(Intent.createChooser(intent, null))
         }
     }
 }
